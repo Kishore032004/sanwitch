60efaf14192509521741f88b15f515ef0ff44fa8
Kishore032004 <kishore03112004@gmail.com>
2026-07-24 16:13:41 +0530

    Update Sanwitch IDE project: main.py

---
+++ Distance Measurement Using Ultrasonic Sensor/main.py
@@ -0,0 +1,16 @@
+import time
+from machine import Pin, time_pulse_us
+
+def read_ultrasonic_cm(trig_pin, echo_pin):
+    trig = Pin(trig_pin, Pin.OUT)
+    echo = Pin(echo_pin, Pin.IN)
+    trig.value(0)
+    time.sleep_us(2)
+    trig.value(1)
+    time.sleep_us(10)
+    trig.value(0)
+    duration = time_pulse_us(echo, 1, 30000)
+    return duration / 58
+
+while True:
+    print(read_ultrasonic_cm(5, 18))
