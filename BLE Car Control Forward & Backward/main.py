ea4f964cae62f5c3de8e9badaf780cdc8c14bcf5
Kishore032004 <kishore03112004@gmail.com>
2026-08-04 12:11:31 +0530

    Update Sanwitch IDE project: main.py

---
+++ BLE Car Control Forward & Backward/main.py
@@ -0,0 +1,27 @@
+import bluetooth
+from machine import Pin
+
+from ble_uart import BLEUART
+import bluetooth
+_ble = bluetooth.BLE()
+_uart = BLEUART(_ble, name="Sanwitch-ESP32")
+while True:
+    msg = (_uart.read().decode().strip() if _uart.any() else "")
+    if msg == "FRONT :PUSH":
+        print("Front  Pressed")
+        Pin(26, Pin.OUT).value(1)
+        Pin(27, Pin.OUT).value(0)
+        Pin(26, Pin.OUT).value(1)
+        Pin(27, Pin.OUT).value(0)
+    if msg == "STOP :PUSH":
+        print("Stop  Pressed")
+        Pin(26, Pin.OUT).value(0)
+        Pin(27, Pin.OUT).value(0)
+        Pin(26, Pin.OUT).value(0)
+        Pin(27, Pin.OUT).value(0)
+    if msg == "BACK:PUSH":
+        print("Back Pressed")
+        Pin(26, Pin.OUT).value(0)
+        Pin(27, Pin.OUT).value(1)
+        Pin(26, Pin.OUT).value(0)
+        Pin(27, Pin.OUT).value(1)
